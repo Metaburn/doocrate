8f0e4629634f1e2ee4c3bb543bf411fbf08c4d09
Gal Bracha <galbra@gmail.com>
2018-08-21 14:59:45 +0300

    Setting the main page as initial page

--- src/firebase/config.production.js
+++ src/firebase/config.production.js
@@ -5,5 +5,6 @@ export const firebaseConfig = {
   storageBucket: '',
   projectId: 'doocrate-2018',
   // This loads the Event planning page - Set to 2,3 to change it
-  defaultPageToLoad: '?filter=taskType&text=1'
+  // Or leave it empty to show all the tasks as the default page
+  defaultPageToLoad: '' //'?filter=taskType&text=3'
 };
