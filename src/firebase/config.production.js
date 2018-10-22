7de9a1bef41fec4e470dcfba0869b3f188cb30a9
Gal Bracha <galbra@gmail.com>
2018-10-22 15:14:47 +0300

    Changed urls to doocrate production

--- src/firebase/config.production.js
+++ src/firebase/config.production.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCMeF8iiKlFpsNheQD3dhVRGb_5Rs2JX3k',
-  authDomain: 'doocrate-2018.firebaseapp.com',
-  databaseURL: 'https://doocrate-2018.firebaseio.com',
+  apiKey: 'AIzaSyDyNtWKfWgdyfR4mJ0MoFtcEtEP2mYAuj0',
+  authDomain: 'doocrate-production.firebaseapp.com',
+  databaseURL: 'https://doocrate-production.firebaseio.com',
   storageBucket: '',
-  projectId: 'doocrate-2018',
+  projectId: 'doocrate-production\n',
   // This loads the Event planning page - Set to 2,3 to change it
   // Or leave it empty to show all the tasks as the default page
   defaultPageToLoad: '' //'?filter=taskType&text=3'
