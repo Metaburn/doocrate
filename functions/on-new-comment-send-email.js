bcea73030ec3056c6982a4639b30a07bd31a11d4
Gal Bracha <galbra@gmail.com>
2019-09-23 19:53:39 -0600

    Looking at the users language to send emails - TODO - test this

--- functions/on-new-comment-send-email.js
+++ functions/on-new-comment-send-email.js
@@ -1,9 +1,8 @@
-//----------------SendEmail----------------
+import {firebaseDb} from "../src/firebase";
+
 const functions = require('firebase-functions');
-// TODO: Configure the `email.from`, `send_notifications`, `email.apikey`, `email.domain` Google Cloud environment variables.
-// For example: firebase functions:config:set email.send_notifications="true"
-// For example: firebase functions:config:set email.apiKey=KEY
-// For example: firebase functions:config:set email.domain=DOMAIN
+import newCommentEn from './templates/new-comment-en';
+import newCommentHe from './templates/new-comment-he';
 const emailConfig = functions.config().email;
 const shouldSendNotifications = emailConfig? encodeURIComponent(emailConfig.send_notifications) : false;
 const fromEmail = emailConfig? decodeURIComponent(emailConfig.from) : null;
@@ -17,80 +16,93 @@ const firestore = new Firestore();
 firestore.settings({
   timestampsInSnapshots: true
 });
+
+
 /*
   Whenever a new comment is created - an email is sent
  */
 exports.onNewCommentSendEmail = functions.firestore.document('/projects/{projectId}/comments/{commentId}').onWrite((change, context)=> {
 
   if(!shouldSendNotifications) {
-    console.warn('send notifications turned off');
+    console.warn('Send notifications turned off');
     return false;
   }
 
   const projectId = context.params.projectId;
-
-  console.log('from:' + fromEmail);
-
+  console.log('From:' + fromEmail);
   const comment = change.after.data();
 
   // Check for deleted comment
   if(!comment || !comment.taskId) {
-    return
+    return;
   }
 
   return firestore.collection('projects').doc(projectId).collection('tasks').doc(comment.taskId).get().then( taskSnapshot => {
-      const task = taskSnapshot.data();
-      if(!task || !task.creator || !task.creator.email) {
-        console.log('No email found');
-        return;
+    const task = taskSnapshot.data();
+    if(!task || !task.creator || !task.creator.email) {
+      console.log('No email found');
+      return;
+    }
+
+    function getUserInfo(userId) {
+      return firebaseDb.collection('users').doc(userId).get();
+    }
+
+    function getEmailParams(toEmail, language) {
+      const mailOptions = {
+        from: comment.creator.name + ' ' + fromEmail, // For example Gal Bracha <support@doocrate.com>
+        to: toEmail,
+        'h:Reply-To': comment.creator.email
+      };
+      let emailTemplate;
+
+      const templateData = {
+        fromName: comment.creator.name,
+        fromEmail: comment.creator.email,
+        fromPhotoUrl: comment.creator.photoURL,
+        body: comment.body,
+        link: `https://doocrate.com/${projectId}/task/${comment.taskId}}`
+      };
+
+      const shortTitle = task.title.substr(0, 20);
+      if (language === 'he') {
+        mailOptions.subject = `תגובה חדשה - [${shortTitle}]`;
+        emailTemplate = newCommentHe(templateData);
+      }else { //English
+        mailOptions.subject = `New Comment - [${shortTitle}]`;
+        emailTemplate = newCommentEn(templateData);
       }
 
-      function getEmailParams(toEmail) {
-        const mailOptions = {
-          from: comment.creator.name + ' ' + fromEmail, // For example Gal Bracha <support@doocrate.com>
-          to: toEmail,
-          'h:Reply-To': comment.creator.email
-        };
+      mailOptions.html = emailTemplate;
+      return mailOptions;
+    }
 
+    // Get the user language and send the email
+    const userPromises = [];
+    const creatorEmail = task.creator.email;
+    userPromises.push(getUserInfo(creatorEmail));
+    if(task.assignee && task.assignee.email && task.assignee.email !== creatorEmail) {
+      userPromises.push(getUserInfo(task.assignee.email));
+    }
 
-        const emailTemplate = `<div style="direction:rtl;"><h2>הערה חדשה</h2>
-        <span>
-        מאת: 
-        ${comment.creator.name} ${comment.creator.email}
-        </span>
-        <div><img src='${comment.creator.photoURL}' style='display:block; border-radius:70px;width:140px;height:140px;'/></div><br/>
-        
-        <button style='background:#eb1478;cursor: pointer;color: white;padding:0.7em;font-size:0.8em;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;margin:20px'>
-          <a style='text-decoration: none;color: white' href='https://doocrate.com/${projectId}/task/${comment.taskId}'>
-          לחץ כאן למעבר למשימה
-          </a>
-        </button>
-        <h3>תוכן: ${comment.body}</h3> <br/>
-        <br>אם ברצונך להסיר את עצמך מנוטיפקציות כאלו. אנא שלח אימייל ל-support@doocrate.com
-        <br>        דואוקרט
-        </div>
-      `;
-
-        const shortTitle = task.title.substr(0, 20);
-        mailOptions.subject = `הערה חדשה - [${shortTitle}]`;
-        mailOptions.html = emailTemplate;
-        return mailOptions;
-      }
-
-      const creatorEmail = task.creator.email;
-      let promises = [];
-
-      promises.push(mailgun.messages().send(getEmailParams(creatorEmail)));
-      if(task.assignee && task.assignee.email && task.assignee.email !== creatorEmail) {
-        promises.push(mailgun.messages().send(getEmailParams(task.assignee.email)));
+    // TODO replace with await
+    return Promise.all(userPromises).then(usersData => {
+      const mailPromises = [];
+      console.log(usersData);
+      const languageCreator = usersData[0].language || 'he'; //Defaults to hebrew;
+      mailPromises.push(mailgun.messages().send(getEmailParams(usersData[0].email,languageCreator)));
+      if(usersData.length > 1) {
+        const languageAssignee = usersData[1].language || 'he'; //Defaults to hebrew;
+        mailPromises.push(mailgun.messages().send(getEmailParams(usersData[1].email, languageAssignee)));
       }
 
       return Promise.all(promises).then(values => {
-        console.log('email sent successfully');
+        console.log('Email sent successfully');
         return true;
       }).catch(error => {
-        console.error('error sending mail:', error);
+        console.error('Error sending mail:', error);
         return error;
       });
+    });
     }
   )});
---
+++ functions/templates/new-comment-he.js
@@ -0,0 +1,23 @@
+export default function newCommentHe(args) {
+  const {fromName, fromEmail, fromPhotoUrl, body, link} = args;
+
+  const template = `<div style="direction:ltr;"><h2>תגובה חדשה</h2>
+        <span>
+        מאת:
+        ${fromName} ${fromEmail}
+        </span>
+        <div><img src='${fromPhotoUrl}' style='display:block; border-radius:70px;width:140px;height:140px;'/></div><br/>
+        
+        <button style='background:#eb1478;cursor: pointer;color: white;padding:0.7em;font-size:0.8em;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;margin:20px'>
+          <a style='text-decoration: none;color: white' href='${link}'>
+          לחץ כאן למעבר למשימה
+          </a>
+        </button>
+        <h3>תוכן: ${body}</h3> <br/>
+        <br>אם ברצונך להסיר את עצמך מנוטיפקציות כאלו. אנא שלח אימייל ל-support@doocrate.com
+        <br>        דואוקרט
+        </div>
+      `;
+
+  return template;
+}
