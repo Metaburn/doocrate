8ad4eef8f923bc2ade613df67c4a3008b1ccc7f6
Gal Bracha <galbra@gmail.com>
2019-10-27 21:07:08 -0500

    All kittens are now encrypted :)

--- src/views/components/about-carousel/about-carousel.js
+++ src/views/components/about-carousel/about-carousel.js
@@ -20,63 +20,63 @@ class AboutCarousel extends React.Component {
     let humans = [{
       name_he:'גל ברכה',
       name_en: 'Gal Bracha',
-      img:'http://placekitten.com/550/350?image=4'
+      img:'https://placekitten.com/550/350?image=4'
     },{
       name_he:'מתן זוהר',
       name_en:'Matan Zohar',
-      img:'http://placekitten.com/550/350?image=8'
+      img:'https://placekitten.com/550/350?image=8'
     },{
       name_he:'דניאל ברוקס',
       name_en:'Daniel Broks',
-      img:'http://placekitten.com/550/350?image=10'
+      img:'https://placekitten.com/550/350?image=10'
     },{
       name_he:'אור גרנות',
       name_en:'Or Granot',
-      img:'http://placekitten.com/550/350?image=15'
+      img:'https://placekitten.com/550/350?image=15'
     },{
       name_he:'רקפת בר סלע',
       name_en:'Rakefet Bar Sela',
-      img:'http://placekitten.com/550/350?image=11'
+      img:'https://placekitten.com/550/350?image=11'
     },{
       name_he:'הילה היילו',
       name_en:'Hila Halo Lor',
-      img:'http://placekitten.com/550/350?image=2'
+      img:'https://placekitten.com/550/350?image=2'
     },{
       name_he:'יערה מסיקה',
       name_en:'Yarra Mesika',
-      img:'http://placekitten.com/550/350?image=12'
+      img:'https://placekitten.com/550/350?image=12'
     },{
       name_he:'הילה זני',
       name_en:'Hila Zeni',
-      img:'http://placekitten.com/550/350?image=13'
+      img:'https://placekitten.com/550/350?image=13'
     },{
       name_he:'יונתן רוסאק',
       name_en:'Yonatan Rossak',
-      img:'http://placekitten.com/550/350?image=9'
+      img:'https://placekitten.com/550/350?image=9'
     },{
       name_he:'רותם בונדר',
       name_en:'Rotem Bonder',
-      img:'http://placekitten.com/550/350?image=14'
+      img:'https://placekitten.com/550/350?image=14'
     },{
       name_he:'טל לותן',
       name_en:'Tal Lotan',
-      img:'http://placekitten.com/550/350?image=3'
+      img:'https://placekitten.com/550/350?image=3'
     },{
       name_he:'הילה ברזילי',
       name_en:'Hila Barzilai',
-      img:'http://placekitten.com/550/350?image=16'
+      img:'https://placekitten.com/550/350?image=16'
     },{
       name_he:'Nate',
       name_en:'Nate',
-      img:'http://placekitten.com/550/350?image=5'
+      img:'https://placekitten.com/550/350?image=5'
     },{
       name_he:'אורי קדוש',
       name_en:'Ori Kadosh',
-      img:'http://placekitten.com/550/350?image=18'
+      img:'https://placekitten.com/550/350?image=18'
     },{
       name_he:'ניר בניטה',
       name_en:'Nir-Benita',
-      img:'http://placekitten.com/550/350?image=8'
+      img:'https://placekitten.com/550/350?image=8'
     }
     ];
 
