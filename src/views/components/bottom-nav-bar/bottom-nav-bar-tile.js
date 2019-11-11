23a460c5023bb21575c28727d5c02db97608bd32
guyavraham <guy1812@gmail.com>
2019-11-11 09:20:14 +0200

    manage the search query on redux

--- src/views/components/bottom-nav-bar/bottom-nav-bar-tile.js
+++ src/views/components/bottom-nav-bar/bottom-nav-bar-tile.js
@@ -1,15 +1,19 @@
-import React, {Fragment} from 'react';
-import { Link } from 'react-router-dom';
-import Icon from "../../atoms/icon"
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import Icon from "../../atoms/icon";
 
-export default ({icon, path, active, dataTour}) => {
-  const activeClass = active? 'active': '';
+export default ({ icon, path, active, dataTour, onClick }) => {
+  const activeClass = active ? "active" : "";
   return (
     <Fragment>
-      <Link className={`bottom-nav-tile ${activeClass}`} to={path} data-tour={dataTour}>
-        <Icon className={'bottom-nav-icon'} name={icon}/>
+      <Link
+        className={`bottom-nav-tile ${activeClass}`}
+        to={path}
+        data-tour={dataTour}
+        onClick={onClick}
+      >
+        <Icon className={"bottom-nav-icon"} name={icon} />
       </Link>
-
     </Fragment>
-  )
-}
+  );
+};
