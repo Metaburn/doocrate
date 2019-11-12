95ed7cc7b7e603d050cd1cd31494a1d36301c028
Yoni Weizenberg <yoni@lannister.group>
2019-11-12 21:27:33 +0200

    Added function to set user id after invitation

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,33 @@
-'use strict';
+"use strict";
 
 // Max number of tasks per creator
-const functions = require('firebase-functions');
-exports.onNewProjectMakeAdminFunctions = require('./firestore_hooks/on-new-project-make-admin');
-exports.onNewCommentSendEmail = require('./firestore_hooks/on-new-comment-send-email');
-exports.onEditTaskSendEmail = require('./firestore_hooks/on-edit-task-send-email');
-exports.api = require('./doocrateApi').app;
+const functions = require("firebase-functions");
+exports.onNewProjectMakeAdminFunctions = require("./firestore_hooks/on-new-project-make-admin");
+exports.onNewCommentSendEmail = require("./firestore_hooks/on-new-comment-send-email");
+exports.onEditTaskSendEmail = require("./firestore_hooks/on-edit-task-send-email");
+exports.onNewInviteCheckUser = require("./firestore_hooks/on-new-invite-check-user");
+exports.api = require("./doocrateApi").app;
 
 const MAX_TASK_PER_CREATOR = 80;
 // Limit the number of tasks by creator
-exports.limitTasksPerCreatorFirestore = functions.firestore.document('/tasks/{taskId}').onCreate((snap, context) => {
-  console.log("On create");
-  const parentRef = snap.ref.parent;
-  const task = snap.data();
+exports.limitTasksPerCreatorFirestore = functions.firestore
+  .document("/tasks/{taskId}")
+  .onCreate((snap, context) => {
+    console.log("On create");
+    const parentRef = snap.ref.parent;
+    const task = snap.data();
 
-  // If delete occur or this is an existing record or no creator
-  if (!task || !task.creator || !task.creator.id)
-    return;
+    // If delete occur or this is an existing record or no creator
+    if (!task || !task.creator || !task.creator.id) return;
 
-  const creatorId = task.creator.id;
+    const creatorId = task.creator.id;
 
-  return parentRef.where('creator.id',"==", creatorId).get().then(snapshot => {
-
-    if(snapshot.size >= MAX_TASK_PER_CREATOR) {
-      return snap.ref.delete();
-    }
+    return parentRef
+      .where("creator.id", "==", creatorId)
+      .get()
+      .then(snapshot => {
+        if (snapshot.size >= MAX_TASK_PER_CREATOR) {
+          return snap.ref.delete();
+        }
+      });
   });
-});
