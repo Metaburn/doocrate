d83fad42c5491633769f4d934863ed40b60066a8
guyavraham <guy1812@gmail.com>
2019-11-12 21:33:30 +0200

    reverting the condition that starts task filtering

--- src/views/pages/tasks/tasks-page.js
+++ src/views/pages/tasks/tasks-page.js
@@ -96,8 +96,10 @@ export class TasksPage extends Component {
     // To prevent a race condition we want to make sure that only
     // when there are no tasks - we don't update those filters
     // This allows to have the user loads a page directly with filters
-    if(nextSize && (nextSize !== prevSize || !is(nextProps.tasks, tasks)) && JSON.stringify(nextFilters) !== JSON.stringify(selectedFilters)) {
+    if(nextProps.tasks && nextProps.tasks.size > 0) {
+      if(JSON.stringify(nextFilters) !== JSON.stringify(selectedFilters)) {
         this.debouncedFilterTasksFromProps(nextProps);
+      }
     }
 
     //if url has a task id - select it
