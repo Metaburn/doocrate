7e94b77d1bc598f8098d10d0109b3d958d6564cf
Gal Bracha <galbra@gmail.com>
2020-07-30 14:11:09 +0300

    Restored public projects

--- functions/api/auth/auth.js
+++ functions/api/auth/auth.js
@@ -14,23 +14,39 @@ exports.project_permissions = async (req, res) => {
   console.log(req.query);
   console.log(req.user);
 
-  const {firestore} = req.app.locals;
-  const {project} = req.query;
-  const {user} = req;
+  const { firestore } = req.app.locals;
+  const { project } = req.query;
+  const { user } = req;
 
   // TODO - Sanitize project to protect against bad characters
 
-  if(project === undefined) {
-    return res.status(400).send(JSON.stringify({error:"Project is invalid"}))
+  if (project === undefined) {
+    return res
+      .status(400)
+      .send(JSON.stringify({ error: 'Project is invalid' }));
+  }
+
+  const project = await firestore.collection('projects').doc(project);
+
+  // Public project are internet wide public - this is here till we implement an invites system
+  if (project.isPublic === true) {
+    const payload = {
+      canAdd: true,
+      canAssign: true,
+      canComment: true,
+      canView: true,
+    };
+    return res.send(JSON.stringify(payload));
   }
 
   const inviteList = await firestore
-    .collection("projects")
+    .collection('projects')
     .doc(project)
-    .collection("invitation_lists");
+    .collection('invitation_lists');
 
   const inviteListResponse = await inviteList
-    .where("invites", "array-contains", user.email).get();
+    .where('invites', 'array-contains', user.email)
+    .get();
 
   const inviteListResultSize = inviteListResponse.size;
 
@@ -40,7 +56,7 @@ exports.project_permissions = async (req, res) => {
     canAdd: haveAccess,
     canAssign: haveAccess,
     canComment: haveAccess,
-    canView: haveAccess
+    canView: haveAccess,
   };
 
   res.send(JSON.stringify(payload));
